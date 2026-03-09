14a87ef899e8f77341f1c517db489e582402ebdb
KOUSHIK C M <koushikmanohar83@gmail.com>
2026-03-09 20:32:59 +0530

    Update slave_apb.v

--- ON-CHIP PROTOCOLS/APB/DESIGN/slave_apb.v
+++ ON-CHIP PROTOCOLS/APB/DESIGN/slave_apb.v
@@ -2,9 +2,9 @@ module slave_apb #( parameter N=8,A=16)
   (
     input PCLK,
     input PRESETn,
-    input [N-1:0] PWDATA, 
-    input [A-1:0]PADDR,
-    input [2:0]PSEL,
+    input [N-1:0]PWDATA, 
+    input [A-5:0]PADDR,
+    input PSEL,
     input PWRITE,
     input PENABLE,
     output reg PREADY,
@@ -12,90 +12,31 @@ module slave_apb #( parameter N=8,A=16)
     output reg PSLVERR
   );
   localparam integer loc=(1<<(A-4));
-  reg [N-1:0]mem0[0:loc-1];
-  reg [N-1:0]mem1[0:loc-1];
-  reg [N-1:0]mem2[0:loc-1];
-  reg [N-1:0]mem3[0:loc-1];
-  reg [N-1:0]mem4[0:loc-1];
-  reg [N-1:0]mem5[0:loc-1];
-  reg [N-1:0]mem6[0:loc-1];
-  reg [N-1:0]mem7[0:loc-1];
+  reg [N-1:0]mem[0:loc-1];
   always @(posedge PCLK)begin
   if(!PRESETn)begin
       PREADY<=0;
     end
  else begin
-   if(!PENABLE && !PWRITE) 
-        PREADY<=1;
-   else if(!PENABLE && PWRITE) 
-        PREADY<=1;
-   else if(PENABLE && !PWRITE) begin
-        PREADY<=1;  
-    case(PSEL)
-      3'b000:begin
-        PRDATA<=mem0[PADDR[A-5:0]];
-        end
-      3'b001:begin
-        PRDATA<=mem1[PADDR[A-5:0]];
-         end
-      3'b010:begin
-        PRDATA<=mem2[PADDR[A-5:0]];
-         end
-      3'b011:begin
-        PRDATA<=mem3[PADDR[A-5:0]];
-         end
-      3'b100:begin
-        PRDATA<=mem4[PADDR[A-5:0]];
-         end
-      3'b101:begin
-        PRDATA<=mem5[PADDR[A-5:0]];
-         end
-      3'b110:begin
-        PRDATA<=mem6[PADDR[A-5:0]];
-         end
-      3'b111:begin
-        PRDATA<=mem7[PADDR[A-5:0]];
-         end
-       default PRDATA<=8'B0;
-    endcase
-   end
-  else if(PENABLE && PWRITE) begin
-        PREADY<=1;  
-    case(PSEL)
-      3'b000:begin
-        mem0[PADDR[A-5:0]]<=PWDATA;
-        end
-      3'b001:begin
-        mem1[PADDR[A-5:0]]<=PWDATA;
-         end
-      3'b010:begin
-        mem2[PADDR[A-5:0]]<=PWDATA;
-         end
-      3'b011:begin
-        mem3[PADDR[A-5:0]]<=PWDATA;
-         end
-      3'b100:begin
-        mem4[PADDR[A-5:0]]<=PWDATA;
-         end
-      3'b101:begin
-        mem5[PADDR[A-5:0]]<=PWDATA;
-         end
-      3'b110:begin
-        mem6[PADDR[A-5:0]]<=PWDATA;
-         end
-      3'b111:begin
-        mem7[PADDR[A-5:0]]<=PWDATA;
-         end
-    endcase
+   if(!PSEL) begin
+     PRDATA<=PRDATA;
+     PREADY<=1'b1;
+    end
+   else begin
+       PREADY<=1;
+     case({PENABLE,PWRITE})
+       2'b10:PRDATA<=mem[PADDR[A-5:0]]; 
+       2'b11:mem[PADDR[A-5:0]]<=PWDATA;
+       default:PRDATA<=PRDATA;
+      endcase
    end
   end
  end
-always @(posedge PCLK or negedge PRESETn)
-begin
+always @(posedge PCLK or negedge PRESETn)begin
   if(!PRESETn)
      PSLVERR<=0;
   else if(PENABLE) begin
-      if(PADDR[A-5:0]>=loc)
+    if(PADDR[A-5:0]>=loc || PADDR[A-5:0]=={A{1'bx}})
          PSLVERR<=1;
       else
          PSLVERR<=0;
